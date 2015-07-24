00f7d75e31691566cdc8741a755dcaf29a42ced2
茄籽 <kechun.wkc@alibaba-inc.com>
2015-07-24 11:40:09 +0800

    导航

--- src/views/Test.js
+++ src/views/Test.js
@@ -37,73 +37,6 @@ var Nav = React.createClass({
     }
 });
 
-function sleep(cb ,time){
-    setTimeout(cb,time)
-}
-
-//  + Math.random()/3
-var App1 = React.createClass({
-    componentDidMount(){
-        var sel = document.querySelector.bind(document)
-        var sela = document.querySelectorAll.bind(document)
-        var canvas = sel('#canvas')
-        var context = canvas.getContext('2d')
-        context.fillStyle = '#eeeeef'
-        context.fillRect(0, 0, 900, 600)
-        
-        var dig = Math.PI / 15 * 11
-        var dx = 300, dy = 300, s = 200
-        context.beginPath()
-        context.fillStyle  = "rgb(100,255,100)"
-        context.strokeStyle = "rgb(0,0,100)"
-        var x = 0, y = 1;
-        for(var i=0; i<30; i++){
-            x = Math.sin(i*dig + Math.random()/3)
-            y = Math.cos(i*dig + Math.random()/3) 
-            context.lineTo(dx + x*s, dy + y*s)
-            context.fill()
-            context.stroke()
-        }
-        context.closePath()
-    },
-    render: ()=> {
-        return (
-            <div className="row">
-                <canvas id="canvas" width="900px" height="600px"></canvas>
-            </div>
-        )
-    }
-});
-// canvas currentTransform direction fillStyle filter font globalAlpha globalCompositeOperation imageSmoothingEnabled lineCap lineDashOffset lineJoin lineWidth miterLimit shadowBlur shadowColor shadowOffsetX shadowOffsetY strokeStyle textAlign textBaseline addHitRegion arc arcTo asyncDrawXULElement beginPath bezierCurveTo clearHitRegions clearRect clip closePath createImageData createLinearGradient createPattern createRadialGradient drawFocusIfNeeded drawImage drawWindow ellipse fill fillRect fillText getImageData getLineDash isPointInPath isPointInStroke lineTo measureText moveTo putImageData quadraticCurveTo rect removeHitRegion resetTransform restore rotate save scale scrollPathIntoView setLineDash setTransform stroke strokeRect strokeText transform translate
-
-//HTML5 Canvas 绘制渐变图形
-var App4 = React.createClass({
-    componentDidMount(){
-        var sel = document.querySelector.bind(document)
-        var sela = document.querySelectorAll.bind(document)
-        var canvas = sel('#canvas')
-        var ctx = canvas.getContext('2d')
-        ctx.fillStyle = '#eeeeef'
-        ctx.fillRect(0, 0, 900, 600)
-        var g1 = ctx.createLinearGradient(0,0,0,300)
-        g1.addColorStop(0,"rgb(255,255,0")
-        g1.addColorStop(1,"rgb(0,255,255")
-        ctx.fillStyle = g1
-        ctx.fillRect(0,0,500,500)
-        
-    },
-    render: ()=> {
-        return (
-            <div className="row">
-                <canvas id="canvas" width="900px" height="600px"></canvas>
-            </div>
-        )
-    }
-});
-
-
-//http://meiqia.com/
-
 
 class Main extends React.Component {
     render() {
@@ -112,10 +45,7 @@ class Main extends React.Component {
         }
         return (
             <div>
-                {Jade.BrowseHappy()}
-                {Jade.Header()}
-                {Jade.Balloon()}
-                {Jade.Section()}
+                {Jade.header()}
             </div>
         )
     }
